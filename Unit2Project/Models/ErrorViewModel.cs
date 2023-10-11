805784a6fa6d43e8863e73f501733fc53524838b
Kylan Larson <kylan.larson@southeasttech.edu>
2023-10-11 16:37:57 -0500

    Added Bootstraps and deleted the old models, view, and controllers

--- Unit2Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Unit2Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
